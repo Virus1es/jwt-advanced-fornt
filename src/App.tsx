96f7589accce890f78e1ffabb2eba08f7c489ad3
Virus1es <vasya.ivanov2004.ivanov@mail.ru>
2026-03-08 21:06:36 +0300

    feat: pin observer and user check auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, {useContext, useEffect} from 'react';
 import LoginForm from "./components/LoginForm";
+import {Context} from "./index";
+import {observer} from "mobx-react-lite";
 
 function App() {
+    const {store} = useContext(Context);
+
+    useEffect(() => {
+        if(localStorage.getItem('refreshToken')) {
+            void store.checkAuth();
+        }
+    }, []);
+
     return (
         <div>
+            <h1>{store.isAuth ? 'Пользователь авторизован' : 'Авторизуйтесь'}</h1>
             <LoginForm/>
         </div>
     );
 }
 
-export default App;
+export default observer(App);
